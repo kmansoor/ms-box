5ef5ee4ff9472bd843b312574f1f292deed7e63f
Kamal Mansoor <km8080@gmail.com>
2021-01-14 22:48:43 -0500

    Added adder-service

---
+++ adder-service/src/test/java/com/example/adderservice/AdderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.adderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
